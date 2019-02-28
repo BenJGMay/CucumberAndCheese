1ae031bcfdec979ab77c045277cfb1ce550dbb72
May <ben.may@mullenloweprofero.com>
2019-02-28 16:01:12 +0000

    Restructured project to allow for the creation of Rspec tests without the Cucumber wrapper, while still using existing PageObjects

--- learn_watir/test_puppies/features/support/env.rb
+++ learn_watir/test_puppies/features/support/env.rb
@@ -2,5 +2,8 @@ require 'rspec'
 require 'page-object'
 require 'data_magic'
 require 'faker'
+require 'require_all'
+
+require_all 'lib'
 
 World(PageObject::PageFactory)
--- learn_watir/test_puppies/lib/routes.rb
+++ learn_watir/test_puppies/lib/routes.rb
@@ -1,5 +1,5 @@
-require 'require_all'
-require_rel 'pages' # adds everything in the pages directory
+# require 'require_all'
+# require_rel 'pages' # adds everything in the pages directory
 
 # We must set a default route, an array of arrays, each of which contains a
 # PageObject and a default method to call = we could add parameters if we wanted
